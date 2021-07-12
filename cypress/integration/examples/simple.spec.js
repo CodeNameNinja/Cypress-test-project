4efe214ef293fc7e9636aef1f0206a01bad25c35
Mitchell <mwhyuen@gmail.com>
2021-07-12 12:36:39 +0200

    feat: add simple tests

---
+++ cypress/integration/examples/simple.spec.js
@@ -0,0 +1,18 @@
+describe('Browser Actions', () => {
+  it('should load correct URL', () => {
+    cy.visit('https://example.com/')
+  })
+
+  it('should check correct URL', () => {
+    cy.url().should('include','example.com')
+    
+  });
+
+  it('should wait for 3 seconds', () => {
+    cy.wait(3000)    
+  });
+
+  it('should check for correct elements on the page', () => {
+    cy.get('h1').should('be.visible');    
+  });
+})
