621533ffce5f74cb1440a58ca8ef55b991c8978e
Mitchell <mwhyuen@gmail.com>
2021-07-13 09:02:13 +0200

    chaining assertions

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -4,18 +4,25 @@ describe('working with inputs', () => {
     cy.get('h3').contains('Log in to ZeroBank')
   });
   it('should fill username', () => {
-    cy.get('#user_login').clear();
-    cy.get('#user_login').type('Mitchell')
+    cy.get('#user_login').as('username');
+    cy.get('@username').clear();
+    cy.get('@username').type('Mitchell', {delay:50})
   });
   it('should fill password', () => {
-    cy.get('#user_password').clear();
-    cy.get('#user_password').type("Mitch007")
+    cy.get('#user_password').as('password')
+    cy.get('@password').clear();
+    cy.get('@password').type("Mitch007", {delay:50})
+  });
+
+  it('should select checkbox', () => {
+    cy.get('[type="checkbox"]').check();
+    cy.wait(1000);
+    cy.get('[type="checkbox"]').should('be.checked')
   });
   it('should submit form', () => {
     cy.get('[type="submit"]').click();
   });
   it('should display error message', () => {
-    cy.get('.alert-error').should('be.visible')
-    cy.get('.alert-error').contains('Login and/or password are wrong.')
+    cy.get('.alert-error').should('be.visible').and('contain', 'Login and/or password are wrong.')
   });
 })
