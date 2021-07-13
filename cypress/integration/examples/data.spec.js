62266fec26163cb0067cf674454a70780b769b8e
Mitchell <mwhyuen@gmail.com>
2021-07-13 20:57:36 +0200

    learnt how to read data from json and txt

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -6,4 +6,11 @@ describe('Write / Read Data to JSON / Text file', function() {
   it('should write data to the text file', () => {
     cy.writeFile('log.txt', 'Hello World');
   });
+
+  it('should read data from the json file', () => {
+     cy.readFile('log.json').its('age').should('eq', 21);
+  });
+  it('should read data from the text file', () => {
+     cy.readFile('log.txt').should('eq', 'Hello World');
+  });
 })
